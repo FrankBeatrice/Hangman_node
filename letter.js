8ef78394a131e115fa508bd2c22fbf9657a0af3a
David Rips <davidrips@github.com>
2016-04-16 13:30:45 -0400

    letter

---
+++ letter.js
@@ -0,0 +1,12 @@
+var Letter = function(let){
+  var charac: this.let,
+  appear: false,
+  var letterRender: function(){
+      if (Letter.appear === false){
+        return "_"
+      }else{return charac}
+  }
+
+}
+
+module.exports = Letter;
